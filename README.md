9434e9064fba5a092b1eba0e968792b1b09ed820
Katsuhiko Nagashima <katsuhiko.nagashima@gmail.com>
2019-03-02 22:47:42 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# cloudwatchalerm-to-slack
+
+CloudWatch アラーム を Slack へ通知します。
+
+参考
+- [CloudWatchアラームとグラフを一緒にSlack通知する](https://qiita.com/hayao_k/items/026e704b5fad3037aea0)
+- [CloudWatchAlarmをグラフ付きでSlackに通知する](https://tech.fusic.co.jp/cloud/cloudwatch-snapshot-graph-to-slack/)
+- [CloudWatch Alarmでメール通知する際に一緒にグラフ画像を添付してみた](https://dev.classmethod.jp/cloud/aws/cloudwatch-snapshot-graph/)
+- [AWS Cloudwatch Snapshot Graphs Alert Context](https://github.com/aws-samples/aws-cloudwatch-snapshot-graphs-alert-context)
+- [Slack APIで画像ファイルをアップロードする](http://iyuichi.hatenablog.jp/entry/2016/06/14/195651)
+- [slack api files.upload](https://api.slack.com/methods/files.upload)
+
+## デプロイ方法
+
+事前に Docker をインストールしてください。
+
+コマンド実行時の重要な引数のみを説明します。
+
+- install
+
+    ```
+    docker run -it -v $(pwd):/home/app -w /home/app node:8.12 npm install
+    ```
+
+- package
+
+    | Argument | Example | Description |
+    |---|---|---|
+    | --s3-bucket | xxx-bucket-sam | そーす等（アーティファクト）をアップロードするバケット |
+
+    ```
+    aws cloudformation package --template-file template.yml --output-template-file serverless-output.yaml --s3-bucket xxx-bucket-sam
+    ```
+
+- deploy
+
+    | Argument | Example | Description |
+    |---|---|---|
+    | SnsTopicName  | xxx-alerm-topic   | アラームを通知する SNS トピック名 |
+    | SlackBotToken | xxx-999-999-xxx   | Slack Bot のトークン |
+    | SlackChannel  | #notify_xxx_alerm | アラームを通知する Slack のチャンネル |
+
+    ```
+    aws cloudformation deploy --template-file serverless-output.yaml --stack-name cloudwatchalerm-to-slack --capabilities CAPABILITY_IAM --region ap-northeast-1 \
+    --parameter-overrides "SnsTopicName=xxx-alerm-topic" "SlackBotToken=xxx-999-999-xxx" "SlackChannel=#notify_xxx_alerm"
+    ```
+
+## デプロイ後の作業
+
+CloudWatch Alerm と結びつけます。
---
+++ index.js
@@ -0,0 +1,112 @@
+'use strict'
+
+const AWS = require('aws-sdk')
+const axios = require('axios')
+const FormData = require('form-data')
+const fs = require('fs')
+const uuidv4 = require('uuid/v4')
+
+const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN
+const SLACK_CHANNEL = process.env.SLACK_CHANNEL
+
+async function getMetricsGraphFromCloudWatch(message) {
+  const props = {
+    width: 480,
+    height: 240,
+    start: '-PT3H',
+    end: 'PT0H',
+    timezone: '+0900',
+    view: 'timeSeries',
+    stacked: false,
+    metrics: [
+      [
+        message.Trigger.Namespace,
+        message.Trigger.MetricName,
+        message.Trigger.Dimensions[0].name,
+        message.Trigger.Dimensions[0].value
+      ]
+    ],
+    stat:
+      message.Trigger.Statistic.charAt(0).toUpperCase() +
+      message.Trigger.Statistic.slice(1).toLowerCase(),
+    period: message.Trigger.Period,
+    annotations: {
+      horizontal: [
+        {
+          color: '#ff6961',
+          label: 'Trouble threshold start',
+          value: 0
+        }
+      ]
+    }
+  }
+  const widgetDefinition = {
+    MetricWidget: JSON.stringify(props)
+  }
+
+  const cloudwatch = new AWS.CloudWatch()
+  try {
+    const response = await cloudwatch
+      .getMetricWidgetImage(widgetDefinition)
+      .promise()
+    return response.MetricWidgetImage
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+async function sendSlackBot(records) {
+  for (const record of records) {
+    const sns = record.Sns
+    const message = JSON.parse(sns.Message)
+
+    let emoji = ':kissing:'
+    if (message.NewStateValue == 'ALARM') {
+      emoji = ':scream:'
+    } else if (message.NewStateValue == 'OK') {
+      emoji = ':grinning:'
+    }
+
+    const image = await getMetricsGraphFromCloudWatch(message)
+
+    // to stream from buffer
+    const file = '/tmp/' + uuidv4() + '-alerm.png'
+    fs.writeFileSync(file, image)
+    const streamImage = fs.createReadStream(file)
+
+    const formData = new FormData()
+    formData.append('token', SLACK_BOT_TOKEN)
+    formData.append('filename', message.AlarmName + '.png')
+    formData.append('file', streamImage)
+    formData.append('filetype', 'png')
+    formData.append('initial_comment', emoji + ' ' + sns.Subject + ', ' + message.NewStateReason)
+    formData.append('channels', SLACK_CHANNEL)
+    formData.append('title', sns.Subject)
+
+    const response = await axios.create({
+      headers: form.getHeaders()
+    }).post('https://slack.com/api/files.upload', formData)
+    console.log(response.data)
+  }
+}
+
+exports.handler = async (event, context) => {
+  // output event
+  console.log(JSON.stringify(event))
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "OK"
+    })
+  }
+
+  try {
+    await sendSlackBot(event.Records)
+  } catch (err) {
+    console.log(err)
+    return err
+  }
+
+  return response
+}
